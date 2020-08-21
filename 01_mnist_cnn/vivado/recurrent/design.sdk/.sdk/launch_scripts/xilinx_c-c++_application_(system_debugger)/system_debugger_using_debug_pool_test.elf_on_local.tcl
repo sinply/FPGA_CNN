464e002857d561928b43ba3410ceea81fe3b16d4
sinply <2322900041@qq.com>
2020-08-21 16:10:24 +0800

    vivado实现整个框架，mnist手写数字识别成功

---
+++ 01_mnist_cnn/vivado/recurrent/design.sdk/.sdk/launch_scripts/xilinx_c-c++_application_(system_debugger)/system_debugger_using_debug_pool_test.elf_on_local.tcl
@@ -0,0 +1,15 @@
+connect -url tcp:127.0.0.1:3121
+source E:/Exercise/FPGA/FPGA_CNN/01_mnist_cnn/vivado/design.sdk/cnn/ps7_init.tcl
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent JTAG-SMT1 210203368853A"} -index 0
+rst -system
+after 3000
+targets -set -filter {jtag_cable_name =~ "Digilent JTAG-SMT1 210203368853A" && level==0} -index 1
+fpga -file E:/Exercise/FPGA/FPGA_CNN/01_mnist_cnn/vivado/design.sdk/cnn/design_1_wrapper.bit
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent JTAG-SMT1 210203368853A"} -index 0
+loadhw E:/Exercise/FPGA/FPGA_CNN/01_mnist_cnn/vivado/design.sdk/cnn/system.hdf
+targets -set -nocase -filter {name =~"APU*" && jtag_cable_name =~ "Digilent JTAG-SMT1 210203368853A"} -index 0
+ps7_init
+ps7_post_config
+targets -set -nocase -filter {name =~ "ARM*#0" && jtag_cable_name =~ "Digilent JTAG-SMT1 210203368853A"} -index 0
+dow E:/Exercise/FPGA/FPGA_CNN/01_mnist_cnn/vivado/design.sdk/pool_test/Debug/pool_test.elf
+bpadd -addr &main
